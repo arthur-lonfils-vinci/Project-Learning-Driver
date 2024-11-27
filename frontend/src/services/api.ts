64aa4d5ecfd0ddbbf955f22f1e6c9b322151c996
Vry0z <arthur.lonfils@gmail.com>
2024-11-27 19:32:50 +0100

    modif: .env for dev

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.33:3010/',
+  baseURL: 'http://localhost:3010/',
   headers: {
     'Content-Type': 'application/json',
   },
